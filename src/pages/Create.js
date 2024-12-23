28f1a558db65ec0e28e2c69ab567bce113ed20de
huzaifaazam7 <muhammadhuzaifaazam7@gmail.com>
2024-12-23 05:21:19 -0800

    initial

---
+++ src/pages/Create.js
@@ -0,0 +1,70 @@
+import { useState } from "react"
+import { Link, Router, useNavigate } from "react-router-dom"
+import supabase from "../config/supabaseClient"
+
+const Create = () => {
+  const navigate = useNavigate()
+
+  const [title, setTitle] = useState('')
+  const [method, setMethod] = useState('')
+  const [rating, setRating] = useState('')
+  const [formError, setFormError] = useState(null)
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    if (!title || !method || !rating) {
+      setFormError('Please fill in all the fields correctly.')
+      return
+    }
+
+    const { data, error } = await supabase
+      .from('smoothies')
+      .insert([{ title, method, rating }])
+
+    if (error) {
+      console.log(error)
+      setFormError('Please fill in all the fields correctly.')
+    }
+    if (data) {
+      console.log(data)
+      setFormError(null)
+      navigate("/second")
+    }
+  }
+
+  return (
+    <div className="page create">
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="title">Title:</label>
+        <input 
+          type="text" 
+          id="title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+
+        <label htmlFor="method">Method:</label>
+        <textarea 
+          id="method"
+          value={method}
+          onChange={(e) => setMethod(e.target.value)}
+        />
+
+        <label htmlFor="rating">Rating:</label>
+        <input 
+          type="number"
+          id="rating"
+          value={rating}
+          onChange={(e) => setRating(e.target.value)}
+        />
+        
+        <button class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Create Smoothie Recipe</button>
+        
+        {formError && <p className="error">{formError}</p>}
+      </form>
+    </div>
+  )
+}
+
+export default Create
